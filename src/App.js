318a983237813015e99535c835552eadd6787cb8
Tony Deng <h.tony.deng@gmail.com>
2021-05-29 20:27:43 -0600

    mvp for adding entry

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState } from "react";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [notes, setNotes] = useState("");
+
+  const handleSubmit = async () => {
+    console.log(JSON.stringify({ title: title, notes: notes }));
+    const response = await fetch("/api/addPrinciple", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title: title, notes: notes }),
+    });
+    console.log(response);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h4>Sick demo principles</h4>
+        <label>Principle title</label>
+        <input
+          type="text"
+          name="principleTitle"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Principle notes</label>
+        <input
+          type="text"
+          name="principleNotes"
+          value={notes}
+          onChange={(e) => setNotes(e.target.value)}
+        />
+        <button onClick={handleSubmit}>Add Principle</button>
       </header>
     </div>
   );
